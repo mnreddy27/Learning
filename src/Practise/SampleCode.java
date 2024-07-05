c124f441f5aaaf6449f137ecba2faedb228d8fe9
narasimhareddy78 <narasimhareddy78@gmail.com>
2024-07-05 17:34:43 +0530

    Simple files

---
+++ src/Practise/SampleCode.java
@@ -0,0 +1,10 @@
+package Practise;
+
+public class SampleCode {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/Practise/SampleCode2.java
@@ -0,0 +1,10 @@
+package Practise;
+
+public class SampleCode2 {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
